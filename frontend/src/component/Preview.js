f13e06cf06678b958e6a3ed51a172be82841a58e
alejandro <alejandro.cena@gmail.com>
2018-08-22 13:51:06 -0300

    Fix in preview

--- frontend/src/component/Preview.js
+++ frontend/src/component/Preview.js
@@ -5,13 +5,13 @@ function Preview ({post,meta}){
     <div>
       <h1>{post.title}</h1>
       <p>
-        <b>Tags: </b> {post.tags.map( tag => <span className="badge badge-info">{tag}</span>)}<br/>
-        <b>Categories: </b> {post.categories.map( category => <span className="badge badge-info">{category}</span>)}
+        <b>Tags: </b> {post.tags.map( (tag,index) => <span key={index} className="badge badge-info">{tag}</span>)}<br/>
+        <b>Categories: </b> {post.categories.map( (category, index) => <span key={index} className="badge badge-info">{category}</span>)}
       </p>
       <img width={'100%'} src={post.image} />
       <p className="lead">{post.dropline}</p>
       <hr className="my-2" />
-      {post.paragraphs.map( paragraph => <p>{paragraph}</p>)}
+      {post.paragraphs.map( (paragraph, index) => <p key={index}>{paragraph}</p>)}
     </div>
   );
 }
